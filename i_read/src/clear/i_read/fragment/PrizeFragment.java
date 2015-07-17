ea82dd3a2ad4242ba2f0b8c2591c2d801a5975c9
miura <miura.tat@gmail.com>
2015-07-17 13:17:05 +0900

    dev

--- i_read/src/clear/i_read/fragment/PrizeFragment.java
+++ i_read/src/clear/i_read/fragment/PrizeFragment.java
@@ -1,138 +1,115 @@
 package clear.i_read.fragment;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
 
-import android.content.Context;
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 import clear.i_read.R;
+import clear.i_read.adapter.SampleAdapter;
+import clear.i_read.adapter.SampleData;
 
-public class PrizeFragment extends Fragment {
+import com.etsy.android.grid.StaggeredGridView;
 
-	private List<String> imgList = new ArrayList<String>();
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+public class PrizeFragment extends Fragment implements
+AbsListView.OnScrollListener, AbsListView.OnItemClickListener {
 
-		View v = inflater.inflate(R.layout.prize_main, container, false);
-		
-		// GridViewのインスタンスを生成
-		GridView gridview = (GridView) v.findViewById(R.id.gridview);
-		// BaseAdapter を継承したGridAdapterのインスタンスを生成
-		// 子要素のレイアウトファイル grid_items.xml を main.xml に inflate するためにGridAdapterに引数として渡す 
-		GridAdapter adapter = new GridAdapter(getActivity(), R.layout.prize_item, imgList);
-		// gridViewにadapterをセット
-		gridview.setAdapter(adapter);
-				
-		getImagePath();
-		
-		return v;
-	}
-	
-	private void getImagePath() {
-		AssetManager assetManager = getResources().getAssets();
-		String[] fileList = null;
-		InputStream input = null;
-		FileOutputStream output = null;
-		String destPath = null;
- 
-		try {
-			fileList = assetManager.list("image");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
- 
-		for(int i=0; i< fileList.length ; i++){
-			try{
-				input = assetManager.open("image/" + fileList[i]);
-				
-				destPath = "/data/data/"+getActivity().getPackageName()+"/" + fileList[i];
-				output=new FileOutputStream(destPath);
- 
-				int DEFAULT_BUFFER_SIZE = 10240 * 4; 
-				byte[] buffer = new byte[DEFAULT_BUFFER_SIZE]; 
-				int n = 0; 
-				while (-1 != (n = input.read(buffer))) { 
-					output.write(buffer, 0, n); 
-				}
-				output.close();
-				input.close();
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
-			// List<String> imgList にはファイルのパスを入れる
-			imgList.add(destPath);
-			
-		}
- 
- 
-	}
-	
-	
-	class ViewHolder {
-		ImageView imageView;
-	}
-	class GridAdapter extends BaseAdapter {
-		private LayoutInflater inflater;
-		private int layoutId;
- 
-		public GridAdapter(Context context,  int layoutId, List<String> imgList) {
-			super();
-			this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			this.layoutId = layoutId; 
-		}
- 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			String mFilepath = imgList.get(position);
- 
-			ViewHolder holder;
-			if (convertView == null) {
-				// main.xml の <GridView .../> に grid_items.xml を inflate して convertView とする
-				convertView = inflater.inflate(layoutId, parent, false); 
-				// ViewHolder を生成
-				holder = new ViewHolder();
-				holder.imageView = (ImageView) convertView.findViewById(R.id.imageview);
-				convertView.setTag(holder);
-			}
-			else {
-				holder = (ViewHolder) convertView.getTag();
-			}
- 
-			Bitmap bmp = BitmapFactory.decodeFile(mFilepath);
-			holder.imageView.setImageBitmap(bmp);
- 
-			return convertView;
-		}
- 
-		@Override
-		public int getCount() {
-			// List<String> imgList の全要素数を返す
-			return imgList.size();
-		}
- 
-		@Override
-		public Object getItem(int position) {
-			return null;
-		}
- 
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
-	}
+	private StaggeredGridView mGridView;
+    private boolean mHasRequestedMore;
+    private SampleAdapter mAdapter;
+
+    private ArrayList<String> mData;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_sgv, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mGridView = (StaggeredGridView) getView().findViewById(R.id.grid_view);
+
+        if (savedInstanceState == null) {
+            final LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+
+            View header = layoutInflater.inflate(R.layout.list_item_header_footer, null);
+            View footer = layoutInflater.inflate(R.layout.list_item_header_footer, null);
+            TextView txtHeaderTitle = (TextView) header.findViewById(R.id.txt_title);
+            TextView txtFooterTitle = (TextView) footer.findViewById(R.id.txt_title);
+            txtHeaderTitle.setText("THE HEADER!");
+            txtFooterTitle.setText("THE FOOTER!");
+
+            mGridView.addHeaderView(header);
+            mGridView.addFooterView(footer);
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new SampleAdapter(getActivity(), R.id.txt_line1);
+        }
+
+        if (mData == null) {
+            mData = SampleData.generateSampleData();
+        }
+
+        for (String data : mData) {
+            mAdapter.add(data);
+        }
+
+        mGridView.setAdapter(mAdapter);
+        mGridView.setOnScrollListener(this);
+        mGridView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onScrollStateChanged(final AbsListView view, final int scrollState) {
+        Log.d("", "onScrollStateChanged:" + scrollState);
+    }
+
+    @Override
+    public void onScroll(final AbsListView view, final int firstVisibleItem, final int visibleItemCount, final int totalItemCount) {
+        Log.d("", "onScroll firstVisibleItem:" + firstVisibleItem +
+                " visibleItemCount:" + visibleItemCount +
+                " totalItemCount:" + totalItemCount);
+        // our handling
+        if (!mHasRequestedMore) {
+            int lastInScreen = firstVisibleItem + visibleItemCount;
+            if (lastInScreen >= totalItemCount) {
+                Log.d("", "onScroll lastInScreen - so load more");
+                mHasRequestedMore = true;
+                onLoadMoreItems();
+            }
+        }
+    }
+
+    private void onLoadMoreItems() {
+        final ArrayList<String> sampleData = SampleData.generateSampleData();
+        for (String data : sampleData) {
+            mAdapter.add(data);
+        }
+        // stash all the data in our backing store
+        mData.addAll(sampleData);
+        // notify the adapter that we can update now
+        mAdapter.notifyDataSetChanged();
+        mHasRequestedMore = false;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item Clicked: " + position, Toast.LENGTH_SHORT).show();
+    }
 }
